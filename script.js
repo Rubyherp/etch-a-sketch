99d01fe0901063ed3e95623e9e0abbce7fd14d72
XN <horxiangneng123@gmail.com>
2026-01-06 00:11:47 +0800

    Add Control section to html. Add resetGrid, and totalReset function. Added a form for flexible changes to grid size

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+let setTimeId = null;
+let submitted = false;
 
 const container = document.querySelector('.container');
+const reset = document.querySelector('.btn-reset');
+const form = document.querySelector('#form');
+const input = document.querySelector('.input-value');
+
+
+container.addEventListener('mouseleave', unchangeColor);
+reset.addEventListener('click', resetGrid);
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    return submitted
+        ? totalReset(input.value)
+        : createGrid(input.value)
+});
 
 function createGrid(size) {
-    document.documentElement.style.setProperty('--size', size);
+    // document.documentElement.style.setProperty('--size', size);
+    submitted = true
     for (let row = 0; row < size; row++ ){
         const rowContainer = document.createElement('div');
         rowContainer.classList.add('row-container');
-
         for (let col = 0; col < size; col++) {
             const div = document.createElement('div');
             div.classList.add('box');
@@ -14,12 +29,9 @@ function createGrid(size) {
             // div.addEventListener('mouseleave', unchangeColor);
             rowContainer.appendChild(div); 
         }
-
         container.appendChild(rowContainer);
     }
 }
-
-let setTimeId = null;
 
 function changeBoxColor(e) {
     clearInterval(setTimeId);
@@ -28,7 +40,7 @@ function changeBoxColor(e) {
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
          e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-         e.target.style.boxShadow = `-10px -3px rgb(${r}, ${g}, ${b}`;
+         e.target.style.boxShadow = `-5px -5px rgb(${r}, ${g}, ${b})`;
     }, 1);
 }
 
@@ -36,19 +48,29 @@ function generateHeaderColor() {
     const r = Math.floor(Math.random()* 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    document.documentElement.setProperty('--r', r);
-    document.documentElement.setProperty('--g', g);
-    document.documentElement.setProperty('--b', b);
+    document.documentElement.style.setProperty('--r', r);
+    document.documentElement.style.setProperty('--g', g);
+    document.documentElement.style.setProperty('--b', b);
 }
 
+function unchangeColor() {
+    clearInterval(setTimeId);
+}
 
+function resetGrid() {
+    const boxes = document.querySelectorAll('.container .box')
+    boxes.forEach(box => {
+        box.style.backgroundColor = 'grey';
+        box.style.boxShadow = 'none';
+    })
+}
 
-// function unchangeColor(e) {
-//     clearInterval(setTimeId);
-//     e.target.style.backgroundColor = `grey`
-// }
+function totalReset(newGrid) {
+    submitted = false;
+    container.innerHTML = '';
+    createGrid(newGrid);
+}
 
 generateHeaderColor();
-createGrid(100);
 
 
